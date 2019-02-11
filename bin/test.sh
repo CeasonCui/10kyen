695e1d04794eab25a1740db2127a9c5d0bbe9b1d
Sykie Chen / Tojo Toshichi <chen17@coder17.com>
2019-02-11 15:49:32 +0800

    fixe path & remove unused file

--- bin/test.sh
+++ bin/test.sh
@@ -3,12 +3,11 @@
 if [ ! -d "./match random zz" ]; then
 
 
-	gcc "match.c" -o match
-	gcc "random.c" -o random
-	gcc "always1000.c" -o always1000
-	gcc "minus1.c" -o minus1
-	gcc "zz.c" -o zz
-	gcc "test.s" -o zz
+	gcc ../match.c -o match
+	gcc ../random.c -o random
+	gcc ../always1000.c -o always1000
+	gcc ../minus1.c -o minus1
+	gcc ../zz.c -o zz
 
 fi
 
