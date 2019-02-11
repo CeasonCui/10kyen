363f0984147ccc36ac933e1e145ca48723d38ab4
HsimWong <16074101@emails.bjut.edu.cn>
2019-02-11 15:38:52 +0800

    modified ./bin/test.sh, compiling process added

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [ ! -d "./match random zz" ]; then
+
+
+	gcc "match.c" -o match
+	gcc "random.c" -o random
+	gcc "always1000.c" -o always1000
+	gcc "minus1.c" -o minus1
+	gcc "zz.c" -o zz
+	gcc "test.s" -o zz
+
+fi
+
 while [ 1 ]; do
     if ! ./match random zz 5; then 
         echo err
